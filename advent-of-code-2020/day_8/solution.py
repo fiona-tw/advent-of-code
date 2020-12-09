8d052db6430734ce1adb9b6f251bf067f4436078
Fiona Tahta-Wraith <f.tahta-wraith@iwoca.co.uk>
2020-12-09 23:29:02 +0000

    implements solution to day 8 PART 2

--- advent-of-code-2020/day_8/solution.py
+++ advent-of-code-2020/day_8/solution.py
@@ -1,14 +1,13 @@
-from re import findall
-
+from copy import deepcopy
 from operator import add, sub
-
-from typing import NamedTuple, List, Union, Type
+from re import findall
+from typing import Dict, List, NamedTuple, Tuple, Union
 
 
 class OperationInput(NamedTuple):
     instruction: 'Instruction'
     accumulator: int
-    current_operation_index: int
+    operation_index: int
     total_num_instructions: int
 
 
@@ -32,17 +31,15 @@ class Op:
 
     def next_operation_index(self, _input: OperationInput) -> int:
         if self.impacts_index:
-            return _input.instruction.direction(_input.current_operation_index, _input.instruction.value)
+            return _input.instruction.direction(_input.operation_index, _input.instruction.value)
 
         # otherwise go to next instruction
-        return _input.current_operation_index + 1
+        return _input.operation_index + 1
 
-    def apply(self, _input: OperationInput) -> OperationResult:
+    def execute(self, _input: OperationInput) -> OperationResult:
         return OperationResult(
             accumulator=self.accumulator(_input),
-            # % to make sure we do not get index out of range error TODO I this even correct?
-            next_operation_index=self.next_operation_index(_input)  # % _input.total_num_instructions,
-
+            next_operation_index=self.next_operation_index(_input)
         )
 
 
@@ -80,10 +77,18 @@ class Instruction(NamedTuple):
     def __repr__(self):
         return f"{self.operation.name} {self.direction.__name__} {self.value}"
 
+    @classmethod
+    def replace_operation(cls, other: 'Instruction', operation: Op):
+        return cls(
+            operation=operation,
+            direction=other.direction,
+            value=other.value,
+        )
+
 
 def load_instructions(filename: str) -> List[Instruction]:
     with open(f"day_8/{filename}", "r") as f:
-        raw_instructions = findall(r"(([a-z]{3}) (\+|-)(\d+)\n)", f.read()[:-1])
+        raw_instructions = findall(r"(([a-z]{3}) (\+|-)(\d+)\n)", f.read())
     return [
         Instruction(
             operation=operations[raw_operation],
@@ -94,7 +99,22 @@ def load_instructions(filename: str) -> List[Instruction]:
     ]
 
 
-def run_instructions(instructions: List[Instruction]):
+class ProgramResult(NamedTuple):
+    running_order: Dict[int, Instruction]
+    accumulator: int
+    has_terminated: bool
+    infinite_loop: bool
+
+
+def get_running_order(record_of_run_instructions: Dict[Tuple[int, Instruction], List[int]]) -> Dict[int, Instruction]:
+    return {
+        run_count: instruction
+        for (index, instruction), run_counts in record_of_run_instructions.items()
+        for run_count in run_counts
+    }
+
+
+def run_program_with_instructions(instructions: List[Instruction], debug: bool = False):
     accumulator = 0
     record_of_run_instructions = {
         (i, instruction): []
@@ -102,37 +122,83 @@ def run_instructions(instructions: List[Instruction]):
     }
     total_num_instructions = len(instructions)
     current_operation_index = 0
-    live = True
-    count = 1
+    current_instruction = instructions[current_operation_index]
+    instruction_uid = (current_operation_index, current_instruction)
+    has_terminated = False
+    infinite_loop = False
+    run_count = 0
 
-    while live:
-        # first thing in while loop should be getting next instruction
-        current_instruction = instructions[current_operation_index]
-        instruction_uid = (current_operation_index, current_instruction)
-        print(f"{current_instruction} \t\t (acc={accumulator}, index={current_operation_index})")
+    while not has_terminated and not infinite_loop:
+        # ########################### LOG CURRENT STATE ###########################
+        if debug:
+            print(f"{current_instruction} \t\t (acc={accumulator}, index={current_operation_index})")
+        run_count += 1
+        record_of_run_instructions[instruction_uid].append(run_count)
 
-        if len(record_of_run_instructions[instruction_uid]) > 0:
-            # i.e. end process as we have reached infinite loop!
-            live = False
-            break
-
-        record_of_run_instructions[instruction_uid].append(count)
-
-        operation_output = current_instruction.operation.apply(OperationInput(
+        # ############################ RUN INSTRUCTION ############################
+        operation_output = current_instruction.operation.execute(OperationInput(
             accumulator=accumulator,
-            current_operation_index=current_operation_index,
+            operation_index=current_operation_index,
             instruction=current_instruction,
             total_num_instructions=total_num_instructions,
         ))
+
+        # ############################ INCREMENT STATE ############################
         accumulator = operation_output.accumulator
 
-        # last thing in while loop should be incrementing index
-        current_operation_index = operation_output.next_operation_index
-        count += 1
+        if operation_output.next_operation_index == total_num_instructions:
+            # i.e. program will attempt to run instruction below last instruction --> termination
+            has_terminated = True
+        elif operation_output.next_operation_index > total_num_instructions:
+            raise ValueError("Program attempting to execute unknown instruction past termination")
+        else:
+            current_operation_index = operation_output.next_operation_index
+            current_instruction = instructions[current_operation_index]
+            instruction_uid = (current_operation_index, current_instruction)
+            if len(record_of_run_instructions[(current_operation_index, current_instruction)]) > 0:
+                # we have encountered the same instruction uid --> must break process to avoid an infinite loop!
+                infinite_loop = True
 
-    return record_of_run_instructions, accumulator
+    return ProgramResult(
+        get_running_order(record_of_run_instructions),
+        accumulator,
+        has_terminated,
+        infinite_loop,
+    )
+
+
+def part_1():
+    result = run_program_with_instructions(load_instructions("input.txt"))
+    print(f"The broken program's accumulator has value \033[1m__{result.accumulator}__\033[0m when it terminates")
+
+
+def part_2():
+    possible_errs = {
+        "jmp": "nop",
+        "nop": "jmp",
+    }
+    broken_instructions = load_instructions("input.txt")
+
+    # idea here is to loop through these and check if program terminates by applying flip to operation type
+    record_of_attempted_fixes = {
+        (i, instruction): False
+        for i, instruction in enumerate(broken_instructions)
+        if instruction.operation.name in possible_errs
+    }
+
+    for index, instruction_to_swap_out in record_of_attempted_fixes.keys():
+        test_instructions = deepcopy(broken_instructions)
+        new_operation = operations[possible_errs[instruction_to_swap_out.operation.name]]
+        test_instructions[index] = Instruction.replace_operation(instruction_to_swap_out, new_operation)
+
+        result = run_program_with_instructions(test_instructions)
+        if result.has_terminated:
+            break
+
+    print(f"The program is fixed by swapping instruction at index {index}: {instruction_to_swap_out}")
+    print(f"At the end of the fixed program, \033[1maccumulator = {result.accumulator}\033[0m")
 
 
 if __name__ == "__main__":
-    _, final_accumulator = run_instructions(load_instructions("input.txt"))
-    print(final_accumulator)
+    part_1()
+    part_2()
